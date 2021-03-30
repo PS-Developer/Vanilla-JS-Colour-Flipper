2e9a863620197fbf0c4022d12e73fbbee9740c62
Prince Shokoya <72402497+PS-Development@users.noreply.github.com>
2021-03-30 22:55:05 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const colours = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".colour");
+
+btn.addEventListener('click', function(){
+    console.log(document.body)
+})
